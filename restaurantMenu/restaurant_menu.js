0430fe79e58fc7a6db906a96c7d514de6ed890cb
Motoko Miyahisa <mmiyahisa@gmail.com>
2024-12-02 13:36:46 -0500

    modified restaurant menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,4 +1,8 @@
 const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
-
+let mainCourseItem = '';
+mainCourseItem.forEach((item, index) => {
+    mainCourseItem += "<p>Item ${index + 1}: ${item}</p>";
+});
+document.getElementById('mainCourseMenuItems').innerHTML = mainCourseItem;
