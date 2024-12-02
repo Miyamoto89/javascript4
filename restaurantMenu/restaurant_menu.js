4c65bd603577b15568d7bb0a0e97d3bf5d48ce30
Motoko Miyahisa <mmiyahisa@gmail.com>
2024-12-02 14:11:00 -0500

    final update on js

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -13,6 +13,6 @@ document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
 
 let dessertItem = '';
 for (let i = 0; i < dessertMenu.length; i++){
-    dessertItem += "<p>Item" + (index + 1) + ":  " + item + "</p>";
+    dessertItem += "<p>Item" + (i + 1) + ":  " + dessertMenu[i] + "</p>";
 }
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
