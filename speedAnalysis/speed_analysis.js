3ddb21e907d955d0d64dc41a7beaae130762ab03
Motoko Miyahisa <mmiyahisa@gmail.com>
2024-12-10 03:32:24 -0500

    speed analysis

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -14,3 +14,36 @@ function startTest(){
     button.innerHTML = "End Test";
     button.onclick = endTest;
 }
+function endTest(){
+    endTime = new Date().getTime();
+
+    //disable user input
+    document.getElementById("userInput").readOnly = true;
+
+    //elapsed time
+    var timeElapsed = (endTime - startTime)/1000;
+    var userTypeText = document.getElementById('userInput').value;
+
+    //split the text
+    var typedWords = userTypeText.split(/\s+/).filter(function(word){
+        return word !== "";
+    }).length;
+
+    var wpm = 0 
+
+    if (timeElapsed !== 0 && !isNaN(typedWords)) {
+        wpm = Math.round((typedWords / timeElapsed) * 60);
+
+    }
+        //display results
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = "<h2>Typing Test Results</h2>" +
+    "<p>Words Typed: " + typedWords + "</p>" +
+    "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" + 
+    "<P>Words Per Minute (WPM)" + typedWords + "WPM</p>"
+
+    //Resert Button
+    var button = document.getElementById("btn");
+    button.innerHTML = "Start Test";
+    button.oncclick = startTest;
+}
