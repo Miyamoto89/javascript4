9428407241d35f575c70bff4f21e5f3c616dc9fc
Motoko Miyahisa <mmiyahisa@gmail.com>
2024-12-12 02:09:54 -0500

    DOM

--- dom_Manipulation/domManip.js
+++ dom_Manipulation/domManip.js
@@ -1,3 +1,13 @@
 const elementsByClass = document.querySelectorAll('.highlighted');
 // Log the selected elements to the console
 console.log(elementsByClass);
+
+document.getElementById('domOutput').innerHTML = elementsByClass;
+
+function item2(){
+
+// Select the element with the ID "my-paragraph" using querySelectorAll
+const elementByID = document.querySelectorAll('#my-paragraph');
+// Log the selected element to the console
+console.log(elementByID);
+}
